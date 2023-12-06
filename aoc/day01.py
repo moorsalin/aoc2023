93d4ee330cb160999270f3f4b9374613d9dfc749
moorsalin <moorsalin.munshi@gmail.com>
2023-12-06 16:21:17 +0000

    include part 2 code

--- aoc/day01.py
+++ aoc/day01.py
@@ -25,6 +25,38 @@ class Solver(aoc.util.Solver):
                 sums += new_num
         return sums
 
+    def replace_word_with_digit(self, input:str):
+        words = {
+            'one':1,
+            'two':2,
+            'three':3,
+            'four':4,
+            'five':5,
+            'six':6,
+            'seven':7,
+            'eight':8,
+            'nine':9,
+        }
+        matches = []
+        for k,_ in words.items():
+            if k in input:
+                input.index
+                match = (k, input.index(k))
+                matches.append(match)
+        matches.sort(key=lambda x: x[1])
+        if matches:
+            for match,_ in matches:
+                input = input.replace(match,f"{words[match]}",1)
+
+        return "".join([i for i in input if i.isdigit()])
+    
     def part_two(self) -> int:
         # TODO: actually return the answer
-        return 0
+        sums = 0
+        for line in self.input.split("\n"):
+            stripped = self.replace_word_with_digit(line)
+            if stripped:
+                new_num = f"{stripped[0]}{stripped[-1]}"
+                new_num = int(new_num)
+                sums += new_nums
+        return sums
